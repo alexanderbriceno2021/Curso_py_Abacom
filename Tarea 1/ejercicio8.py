0e3c70917d9c6b8ec6482d56724621066752277a
alexanderbriceno2021 <github210792@gmail.com>
2021-11-28 18:46:25 -0500

    Ejercicios "Tarea 1"

---
+++ Tarea 1/ejercicio8.py
@@ -0,0 +1,11 @@
+"""
+EJERCICIO 8.-  Escribe un programa que lea un número e indique si es par o impar
+
+"""
+
+numero = int(input("Escribe un numero: "))
+
+if numero % 2 == 0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
