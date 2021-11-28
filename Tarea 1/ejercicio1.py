8685ee099cad826bc146d8cf5f0e24b9e0c5cf1d
alexanderbriceno2021 <github210792@gmail.com>
2021-11-28 18:17:33 -0500

    ejercicios "Tarea 1"

---
+++ Tarea 1/ejercicio1.py
@@ -0,0 +1,10 @@
+""" 
+EJERCICIO 1.- Escribir un programa que pregunte al usuario su nombre, y luego lo salude 
+
+"""
+
+nombre = input("Escriba su nombre: ")
+
+# Convierte la primer letra a mayuscula sin importar como el usuario la ingrese
+hola_nombre = nombre.title()
+print("Hola Bienvenido ", hola_nombre)
